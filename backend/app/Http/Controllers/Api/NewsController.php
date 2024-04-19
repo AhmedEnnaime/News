2a8bfab61eb3f9a5d3be309681bd8c06d2197fb4
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2024-04-19 22:33:34 +0100

    created index method in news controller

--- backend/app/Http/Controllers/Api/NewsController.php
+++ backend/app/Http/Controllers/Api/NewsController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Api\BaseController;
+use App\Http\Resources\NewsCollection;
+use App\Models\News;
 use Illuminate\Http\Request;
 
 class NewsController extends BaseController
 {
-    public function index() {}
+    public function index() {
+        $news = News::with("category")->get();
+        return $this->sendResponse(new NewsCollection($news), "News retrieved successfully", 200);
+    }
 }
