fc219d9250034c0912cd0028df16b93ed93b8737
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2024-04-19 21:59:05 +0100

    created migrations and models relationships

--- backend/app/Models/Category.php
+++ backend/app/Models/Category.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    public function news()
+    {
+        return $this->hasMany(News::class);
+    }
 }
--- backend/database/migrations/2024_04_19_204938_create_news_table.php
+++ backend/database/migrations/2024_04_19_204938_create_news_table.php
@@ -11,13 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('news', function (Blueprint $table) {
             $table->id();
-            $table->string("name");
-            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->string("title");
+            $table->string("content");
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade')->onUpdate('cascade');
+            $table->string("debut_date");
+            $table->string("expiration_date");
             $table->timestamps();
-            $table->foreign('parent_id')->references('id')->on('categories')
-                  ->onDelete('cascade');
         });
     }
 
@@ -26,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('news');
     }
 };
