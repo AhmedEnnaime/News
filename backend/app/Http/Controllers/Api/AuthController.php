ef55927600695b2ceb9462803292508d09fd7a82
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2024-04-20 16:27:41 +0100

    created auth controller and it's methods and routes and protedcted news routes

---
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends BaseController
+{
+    public function register(Request $request){
+        $registerUserData = $request->validate([
+            'name'=>'required|string',
+            'email'=>'required|string|email|unique:users',
+            'password'=>'required|min:8'
+        ]);
+        $user = User::create([
+            'name' => $registerUserData['name'],
+            'email' => $registerUserData['email'],
+            'password' => Hash::make($registerUserData['password']),
+        ]);
+        return response()->json([
+            'message' => 'User Created ',
+            'data' => $user
+        ]);
+    }
+
+    public function login(Request $request){
+        $loginUserData = $request->validate([
+            'email'=>'required|string|email',
+            'password'=>'required|min:8'
+        ]);
+        $user = User::where('email',$loginUserData['email'])->first();
+        if(!$user || !Hash::check($loginUserData['password'],$user->password)){
+            return response()->json([
+                'message' => 'Invalid Credentials'
+            ],401);
+        }
+        $token = $user->createToken($user->name.'-AuthToken')->plainTextToken;
+        return response()->json([
+            'access_token' => $token,
+        ]);
+    }
+
+    public function logout(){
+        auth()->user()->tokens()->delete();
+
+        return response()->json([
+        "message"=>"logged out"
+        ]);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\NewsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,8 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::apiResource("news", NewsController::class);
+Route::apiResource("news", NewsController::class)->middleware('auth:sanctum');
+Route::post('register',[AuthController::class,'register']);
+Route::post('login',[AuthController::class,'login']);
+Route::post('logout',[AuthController::class,'logout'])
+  ->middleware('auth:sanctum');
